5d00f676eac06bf29e14f8e40263b5d95b413405
LaleshGarud <lalesh.garud05@gmail.com>
2026-07-27 13:11:31 +0530

    push all code LG

---
+++ tests/mytest.spec.ts
@@ -0,0 +1,9 @@
+import {test, expect} from '@playwright/test';
+
+test('Verify page title', async ({ page }) => {
+    await page.goto('https://testautomationpractice.blogspot.com/');
+    let title:string = await page.title();
+    console.log('Page title is: ' + title);
+    await expect(page).toHaveTitle('Automation Testing Practice');
+
+} );
