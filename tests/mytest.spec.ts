3c6e94fefd8611c8747471078fd8cd997cac98aa
LaleshGarud <lalesh.garud05@gmail.com>
2026-07-27 13:29:06 +0530

    added github reporter

--- tests/mytest.spec.ts
+++ tests/mytest.spec.ts
@@ -4,6 +4,6 @@ test('Verify page title', async ({ page }) => {
     await page.goto('https://testautomationpractice.blogspot.com/');
     let title:string = await page.title();
     console.log('Page title is: ' + title);
-    await expect(page).toHaveTitle('Automation Testing Practice');
+    await expect(page).toHaveTitle('Automation Testing Practice12345 should be failed');
 
 } );
